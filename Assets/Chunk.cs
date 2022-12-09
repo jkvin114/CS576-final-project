84d5d77f9048d3ee023451385e178eced274d655
jkvin114 <iu889@naver.com>
2022-12-09 13:59:07 -0500

    obstacle generation

--- Assets/Chunk.cs
+++ Assets/Chunk.cs
@@ -1,12 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public enum GridEntry
 {
     EMPTY=0,OBSTACLE=1,LONG_OBSTACLE=2,ENEMY=3
 }
-
+public enum GridInit
+{
+    NONE=0, //obstacle can be placed here
+    GEM=1,  //gems will be placed
+    GEM2 = 2,  //gems will be placed
+    GEM_SPECIAL = 3,
+    EMPTY =4, //no obstacles or gems will be placed here
+    OBSTACLE=5, //obstacle will be placed here
+    OBSTACLES_2LONG = 6,
+    OBSTACLES_3LONG = 7,
+    OBSTACLES_2WIDE = 8,
+    OBSTACLES_3WIDE = 9,
+    LONG_OBSTACLE_SPACE=10,
+    FIXED_OBSTACLE=11,
+    KEY_FOOD=12,
+    OTHER_FOOD=13,
+    ENEMY=14,
+    ENEMY_PATH=15
+}
 public class Chunk
 {
     internal int pos;
@@ -15,65 +34,82 @@ public class Chunk
     private GameObject ground;
     internal float biomeNoise;
     internal float decorationNoise;
-    internal GridEntry[,] grid;
     private Level level;
     internal float[] obstacleScales;
-    public Chunk(int pos, Level level)
+    private Chunk lastChunk;
+    internal List<int> pathEnds;
+
+    private bool isHard=false;
+    private bool fixedGeneration=false;
+    internal int food = -1;
+    public Chunk(int pos, Level level,Chunk lastChunk)
     {
+        this.lastChunk= lastChunk;
         biomeNoise = Mathf.PerlinNoise((pos + Level.biomeSeed) / 20, 0);
         decorationNoise = Mathf.PerlinNoise((pos + Level.decorationSeed) / 10, 0);
         this.pos = pos;
         biome = Biome.GetBiome(pos,level,this);
         biome.chunkpos = pos;
         this.level = level;
-        initializeGrid();
+
+        isHard = Random.Range(0,8)<=level.difficulty-2;
+        //difficulty 0~1 : 0%, difficulty 5: 50%, difficulty 9: 100%
         
     }
-    void initializeGrid()
-    {
 
-        grid = new GridEntry[12, 5];
-        for (int i=0;i<12;i++)
-        {
-            for(int j = 0; j < 5; j++)
-            {
-                grid[i,j] = GridEntry.EMPTY;
-            }
-        }
-        grid[1, 3] = GridEntry.OBSTACLE;
-        grid[3, 1] = GridEntry.OBSTACLE;
-        grid[6, 4] = GridEntry.OBSTACLE;
-        grid[9, 2] = GridEntry.OBSTACLE;
-        grid[7, 0] = GridEntry.OBSTACLE;
-        grid[4, 2] = GridEntry.OBSTACLE;
-        grid[2, 4] = GridEntry.OBSTACLE;
-        grid[11, 0] = GridEntry.OBSTACLE;
-    }
-    Vector3 getCoord(int x, int y)
+    Vector3 getCoord(int x, int lane)
     {
-        return new Vector3(pos * Level.chunkLength + (((float)x -0.5f) / 12.0f) * Level.chunkLength,Level.bottomY, level.laneCoordinates[y]);
+        return new Vector3(pos * Level.chunkLength + (((float)x -0.5f) / 12.0f) * Level.chunkLength,Level.bottomY, level.laneCoordinates[lane]);
     }
-    void addObstacles()
+    Vector3 getItemCoord(int x, int lane)
     {
-        for (int i = 0; i < 12; i++)
+        return new Vector3(pos * Level.chunkLength + (((float)x - 0.5f) / 12.0f) * Level.chunkLength, 1.42f, level.laneCoordinates[lane]);
+    }
+    
+    public void placeGem(int row, int lane,int type)
+    {
+        GameObject gem = level.gem1;
+        if (type == 2) gem = level.gem2;
+        if (type == 3) gem = level.gem3;
+        gem = placeObject(gem, getItemCoord(row, lane), Quaternion.identity);
+        gem.AddComponent<food>();
+    }
+    public void placeFood(int row, int lane, bool isKey)
+    {
+        GameObject food = level.food2;
+        if (isKey) food = level.food1;
+        food = placeObject(food, getItemCoord(row, lane), Quaternion.identity);
+        food.AddComponent<food>();
+    }
+    public void placeObstacle(int obs,int row,int lane)
+    {
+        Quaternion rot = Quaternion.Euler(0, 0, 0);
+        if (obs == 18 || obs == 19 || obs == 11)
         {
-            for (int j = 0; j < 5; j++)
-            {
-                if (grid[i,j] == GridEntry.OBSTACLE)
-                {
-                    Quaternion rot = Quaternion.Euler(0, Random.Range(0, 180), 0);
-                    int obs = biome.getObstacle(i);
-                    //fence
-                    //Debug.Log(obs);
-                    if(obs == 18 || obs == 19)
-                    {
-                     //   Debug.Log("fence");
-                        rot = Quaternion.Euler(0, 90, 0);
-                    }
-                    placeObject(level.obsatcles[obs], getCoord(i, j), rot);
-                }
-            }
+            rot = Quaternion.Euler(0, 90, 0);
         }
+            GameObject obstacle = placeObject(level.obstacles[obs], getCoord(row, lane), rot);
+        obstacle.tag = "obstacle";
+        obstacle.AddComponent<BoxCollider>();
+    }
+    public void placeLongObstacle(int obs, int row, int lane, int length, int width)
+    {
+        Vector3 pos = getCoord(row, lane);
+        int yRot = 0;
+        if (length == 2) pos = (pos + getCoord(row + 1, lane)) / 2.0f;
+        if (length == 3) pos = getCoord(row + 1, lane);
+        if (width == 2) pos = (pos + getCoord(row + 1, lane + 1)) / 2.0f;
+        if (width == 3) pos = getCoord(row, lane + 1);
+
+        if (obs == 5 || obs == 6 || obs == 1 || obs == 2)
+        {
+            yRot=90;
+        }
+        if (length > 1) yRot -= 90;
+            
+        GameObject obstacle = placeObject(level.longObstacles[obs],pos , Quaternion.Euler(0,yRot,0));
+        obstacle.tag = "obstacle";
+        obstacle.AddComponent<BoxCollider>();
     }
     public GameObject placeObject(GameObject prefab, Vector3 pos,Quaternion rot)
     {
@@ -90,7 +126,7 @@ public class Chunk
         }
         Level.DestroyObj(ground);
     }
-    public void generate()
+    public void generate(int foodId)
     {
         Debug.Log(biome.getTerrainObject());
         float startPos = pos * Level.chunkLength;
@@ -100,7 +136,17 @@ public class Chunk
         ground.GetComponent<BoxCollider>().size = new Vector3(1.0f, 1.0f, 1.0f);
         ground.transform.position = new Vector3(startPos, Random.Range(0.001f, 0.0f), 0);
         biome.addFence(startPos);
-        addObstacles();
+
+        List<int> lastPathEnds = new();
+        if (lastChunk != null)
+        {
+            lastPathEnds = lastChunk.pathEnds;
+        }
+        ObstacleGenerator obsgen = ObstacleGenerator.create(pos, level.difficulty, lastPathEnds);
+        obsgen.Generate(Random.Range(0,2)==0);
+        pathEnds = obsgen.pathEnds;
+
+        obsgen.addObstacles(this);
     }
     public void populate()
     {

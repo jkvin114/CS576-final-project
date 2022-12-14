aafe2b579ac1b6620a85bdbdddc08c2d3d0138ef
ericngaik <78914902+ericngaik@users.noreply.github.com>
2022-12-14 14:55:53 -0500

    Added Food List Generator

--- Assets/scripts/Menu.cs
+++ Assets/scripts/Menu.cs
@@ -7,14 +7,17 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
     public GameObject menu;
+    public GameObject tutorial;
 
     public void OpenMenu() {
         menu.gameObject.SetActive(true);
+        tutorial.gameObject.SetActive(true);
         Time.timeScale = 0;
     }
 
     public void CloseMenu() {
         menu.gameObject.SetActive(false);
+        tutorial.gameObject.SetActive(false);
         Time.timeScale = 1;
     }
 }
